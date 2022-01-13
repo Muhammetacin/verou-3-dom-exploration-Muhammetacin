916ce92827c303067734bc26ccc7ca7b242d78c5
Muhammet Cin <muhammetacin@gmail.com>
2022-01-13 14:22:12 +0100

    Complete Chapter 5 ex. 2.1 and 2.2: spacebar event is working + add helper function for log text if the background is too dark

--- 5.Events/script.js
+++ 5.Events/script.js
@@ -10,7 +10,7 @@ for (let actionsquare of actionsquares) {
   actionsquare.addEventListener("click", createColoredBox);
 }
 
-// TODO: Ex.1: Create a new <div> with a class .displayedsquare and the corresponding clicked color in the div above (.displayedsquare-wrapper)
+// TODO: Ex.1.1: Create a new <div> with a class .displayedsquare and the corresponding clicked color in the div above (.displayedsquare-wrapper)
 const sectionWithBoxes = document.querySelector("section:first-child");
 
 function createColoredBox(e) {
@@ -28,7 +28,7 @@ function createColoredBox(e) {
   sectionWithBoxes.appendChild(divSquare);
 }
 
-// TODO: Ex.2: Create a new <li> in the log below to state when the action was done
+// TODO: Ex.1.2: Create a new <li> in the log below to state when the action was done
 const sectionWithLogs = document.querySelector("ul");
 
 function clickOnSquare(e) {
@@ -39,7 +39,79 @@ function clickOnSquare(e) {
   const logColor = e.target.classList[1];
   const logTime = getElapsedTime();
 
-  logItem.appendChild(document.createTextNode("[" + logTime + "] " + "Created a new " + logColor + " square"));
-  
+  logItem.appendChild(
+    document.createTextNode(
+      "[" + logTime + "] " + "Created a new " + logColor.toUpperCase() + " square"
+    )
+  );
+
   sectionWithLogs.appendChild(logItem);
+}
+
+// TODO: Ex.2: Add an event listener on the document <body>, listening for the keypress event.
+const body = document.querySelector("body");
+body.addEventListener("keypress", hitSpacebar);
+body.addEventListener("keypress", logSpacebar);
+
+// TODO: Ex. 2.1: When the spacebar is hit randomly change the background color of the whole page
+function hitSpacebar() {
+  const randomValueR = Math.floor(Math.random() * 255),
+    randomValueG = Math.floor(Math.random() * 255),
+    randomValueB = Math.floor(Math.random() * 255);
+  const randomColor =
+    "rgb(" + randomValueR + ", " + randomValueG + ", " + randomValueB + ")";
+  body.style.backgroundColor = randomColor;
+
+  isLightOrDark(randomValueR, randomValueG, randomValueB);
+}
+
+// TODO: Ex. 2.2: Log when the spacebar is used the same way you used for the generated squares.
+function logSpacebar() {
+  let logSpacebarValue = document.createElement("li");
+  const bgColor = document.body.style.backgroundColor;
+  const logTime = getElapsedTime();
+
+  logSpacebarValue.appendChild(
+    document.createTextNode(
+      "[" + logTime + "] " + "Changed background color to: " + bgColor
+    .toUpperCase())
+  );
+
+  sectionWithLogs.appendChild(logSpacebarValue);
+}
+
+// TODO: Ex. 2.3: When the l key is pressed the log gets deleted (erases the generated <li>s)
+// TODO: Ex. 2.4: When the s key is pressed the squares get deleted (erases the generated squares)
+
+// TODO: Ex. 3: Create a system so that when a user clicks on a generated square an alert pops-up with the color of that square
+
+// function alertColoredBox(e) {
+//   // select every box that are dynamically created
+//   const generatedSquares = document.querySelectorAll(".createdBox");
+//   console.log(generatedSquares[e.classList[2]]);
+// }
+
+// for (let createdsquare of createdSquares) {
+//   createdsquare.addEventListener("click", alertColoredBox);
+// }
+
+
+
+// To change the color of text when background color is too dark
+function isLightOrDark(red, green, blue) {
+  // Variables for red, green, blue values
+  var r = red,
+    g = green,
+    b = blue,
+    hsp;
+
+  // HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
+  hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b));
+
+  // Using the HSP value, determine whether the color is light or dark
+  if (hsp > 127.5) {
+    document.body.style.color = "rgb(15, 15, 15)";
+  } else {
+    document.body.style.color = "rgb(240, 240, 240)";
+  }
 }
