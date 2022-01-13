05a31821e5c25924988dd09b46e61cc8a664866e
Muhammet Cin <muhammetacin@gmail.com>
2022-01-13 10:10:41 +0100

    Chapter 4 ex.1 finished

--- 4.Navigate/script.js
+++ 4.Navigate/script.js
@@ -1,5 +1,12 @@
 // TODO: Ex.1: Select the last child of the <ol> tag and put it at the beginning of the list
 
+let oList = document.querySelector("ol");
+oList.insertBefore(oList.lastElementChild, oList.firstElementChild);
+
+
+
 // TODO: Ex.2: Move the <h2> of the third section in the second one and vice-versa
+
+
 
 // TODO: Ex.3: Delete the last section from the DOM, we don't need it anyways
