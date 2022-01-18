12d075379f89a56b4612f2b71f56696da3062d41
Muhammet Cin <muhammetacin@gmail.com>
2022-01-18 18:23:08 +0100

    Find Fast and Furious. Move it to the top if not first li

---
+++ 7.DOM-drill/script.js
@@ -0,0 +1,34 @@
+// Use childNodes to list all the children from the <ul>
+const childNodesFromList = document.querySelector('ul').childNodes;
+
+
+
+// Write a for loop to iterate over every child. In this loop:
+// Use a condition in the loop to only iterate over element nodes (see: nodeTypes. Use child.nodeType === 1)
+let nrOfItems = 0;
+
+for(let i = 1; i < childNodesFromList.length; i += 2) {   
+    // TODO: Find the element that contains Fast and Furious. If it's not the first element move it to the top of the list
+    if(childNodesFromList[i].innerHTML === "Fast and Furious" && nrOfItems != 0) {
+        childNodesFromList[i].parentNode.insertBefore(childNodesFromList[i], childNodesFromList[0]);
+    }
+    nrOfItems++
+}
+
+// --- FOR LOOP VERSION 2 ---
+// for(let child of childNodesFromList) {
+//     if(child.nodeType === 1 ) {
+//         console.log(child);
+
+//     }
+// }
+
+
+
+
+
+
+// TODO: Since Fast and Furious is the most important franchise ever, add a class .important to the element
+// TODO: Add an eventListener on every item of the list. If the item is clicked an alert() pops up to display the name of clicked element
+// TODO: Add a special condition to the previous function if the clicked item is Fast and Furious the alert() should display The most important franchise ever, the story of DOM(inic) Toretto's family. It's not about car, it's about family.
+// TODO: (*) Remove duplicates using removeChild (duplicates are items with the exact same textContent, isEqualNode might be useful).
