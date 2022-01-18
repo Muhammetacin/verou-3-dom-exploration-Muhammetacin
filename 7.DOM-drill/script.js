f3809020d2e27a0d8f4679e5428f61f9dced698b
Muhammet Cin <muhammetacin@gmail.com>
2022-01-18 18:36:33 +0100

    Change cursor to hand and make li's clickable (alert on click)

--- 7.DOM-drill/script.js
+++ 7.DOM-drill/script.js
@@ -12,9 +12,19 @@ for(let i = 1; i < childNodesFromList.length; i += 2) {
     if(childNodesFromList[i].innerHTML === "Fast and Furious" && nrOfItems != 0) {
         const fastFurious = childNodesFromList[i];
         fastFurious.parentNode.insertBefore(fastFurious, childNodesFromList[0]);
-        // TODO: Since Fast and Furious is the most important franchise ever, add a class .important to the element
+        
+        // Since Fast and Furious is the most important franchise ever, add a class .important to the element
         fastFurious.classList.add("important");
     }
+
+    // --- EXTRA: Change cursor to a hand when you hover over list items to make it look clickable
+    document.body.style.cursor = "pointer";
+
+    // Add an eventListener on every item of the list. If the item is clicked an alert() pops up to display the name of clicked element
+    childNodesFromList[i].addEventListener("click", (event) => {
+        alert(event.target.innerHTML);
+    });
+
     nrOfItems++
 }
 
@@ -29,9 +39,5 @@ for(let i = 1; i < childNodesFromList.length; i += 2) {
 
 
 
-
-
-
-// TODO: Add an eventListener on every item of the list. If the item is clicked an alert() pops up to display the name of clicked element
 // TODO: Add a special condition to the previous function if the clicked item is Fast and Furious the alert() should display The most important franchise ever, the story of DOM(inic) Toretto's family. It's not about car, it's about family.
 // TODO: (*) Remove duplicates using removeChild (duplicates are items with the exact same textContent, isEqualNode might be useful).
